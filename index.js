578215f69f00c3a8afa5335b842541a4d0a1d3dd
Jake Luer <jake@alogicalparadox.com>
2014-03-17 23:27:54 -0400

    add namespace contructor, primary export is factory

--- index.js
+++ index.js
@@ -1,62 +1,40 @@
+/*!
+ * Module dependencies
+ */
 
 var debug = require('sherlock')('struct');
-var inherits = require('super');
-var pathval = require('pathval');
+var Hash = require('gaia-hash').Hash;
+var Namespace = require('./lib/namespace').Namespace;
 
-var Schema = exports.Schema = require('./lib/schema').Schema;
+/*!
+ * Primary exports
+ */
 
+var exports = module.exports = createNamespace;
 
-var Struct = exports.Struct = function Struct(attr, opts) {
-  this.__obj = {};
-};
+/*!
+ * Spaces `HashMap`
+ */
 
-Object.defineProperty(Struct, 'type', {
-  get: function() { return this.prototype.__type; },
-  enumerable: true
-});
+var spaces = exports.spaces = new Hash();
 
-Struct.extend = function(type, _schema) {
-  if (!(type && 'string' === typeof type)) {
-    _schema = type;
-    type = 'Struct';
-  }
+/*!
+ *
+ */
 
-  var Klass = (function(self, schema) {
-    return function() {
-      this.schema = schema;
-      self.apply(this, arguments);
-    };
-  })(this, new Schema(_schema));
+exports.Namespace = Namespace;
+exports.Struct = require('./lib/struct').Struct;
+exports.Schema = require('./lib/schema').Schema;
 
-  inherits.merge([ Klass, this ]);
-  inherits.inherits(Klass, this);
-  inherits.merge([ Klass.prototype, this.prototype ]);
+/**
+ * Create a namespace.
+ *
+ * @param {String} namespace key
+ */
 
-  Object.defineProperty(Klass, 'type', {
-    get: function() { return this.prototype.__type; },
-    enumerable: true
-  });
-
-  Klass.extend = this.extend;
-  Klass.prototype.__type = type;
-  Klass.prototype.constructor = Klass;
-
-  return Klass;
-};
-
-
-Struct.prototype = {
-
-  constructor: Struct,
-
-  __type: 'Struct',
-
-  get: function(path) {
-    return pathval.get(this.__obj, path);
-  },
-
-  set: function(path, value) {
-    return pathval.set(this.__obj, path, value);
-  }
-
-};
+function createNamespace(key) {
+  var ns = new Namespace(ns);
+  spaces.set(key, ns);
+  debug('(namespace) create: %s', key);
+  return ns;
+}
